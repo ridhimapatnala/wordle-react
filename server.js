5b0dab684c8491e7ca6cb860ccf2ecfe78eb262d
ridhimapatnala <sesharidhimapatnala@gmail.com>
2025-05-18 16:11:13 +0530

    changes added

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'build'))); // Serve React build
 
-// Load data from db.json
+// Load db.json
 const db = JSON.parse(fs.readFileSync('./data/db.json', 'utf-8'));
 
-// Endpoints
+// API Endpoints
 app.get('/solutions', (req, res) => {
   res.json(db.solutions);
 });
@@ -18,6 +21,12 @@ app.get('/letters', (req, res) => {
   res.json(db.letters);
 });
 
+// Serve React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
